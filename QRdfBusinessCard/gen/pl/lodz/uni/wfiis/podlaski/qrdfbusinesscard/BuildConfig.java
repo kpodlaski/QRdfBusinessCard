27c88ab4513bf5625ec659ff628b1ee52769654e
kpodlaski <krzysztof.podlaski@gmail.com>
2013-06-09 16:51:33 +0200

    c

---
+++ QRdfBusinessCard/gen/pl/lodz/uni/wfiis/podlaski/qrdfbusinesscard/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pl.lodz.uni.wfiis.podlaski.qrdfbusinesscard;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
